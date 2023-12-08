0c2bae411dafc970a567613930077360ce85e08e
Nam <dcam@34bigthings.com>
2023-12-08 20:47:53 +0100

    First commit

---
+++ Source/IBTest/IBTest.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class IBTest : ModuleRules
+{
+	public IBTest(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
